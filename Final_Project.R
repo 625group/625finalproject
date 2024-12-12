dc6eab5702a8d448e40e3b3fa64d1fb4a44d73ed
Zoe Lu <zoelu0129@gmail.com>
2024-12-12 18:32:38 -0500

    update some code

--- Final_Project.R
+++ Final_Project.R
@@ -1,86 +1,62 @@
-library(dplyr)
-library(corrplot)
 library(Hmisc)
 library(ggplot2)
 library(psych)
 library(GGally)
-library(vioplot)
 library(DescTools)
-library(leaps)
 library(tidyverse)
 library(caret)
 library(e1071)
 library(rattle)
-library(dplyr)
-library(rpart)
 library(stats)
-library(factoextra)
 library(MASS)
 library(car)
 library(tidyverse)
 library(data.table)
-library(skimr)
-library(randomForest)
 library(ROSE)
 library(tuneRanger)
-library(VSURF)
 library(foreach)
 library(doParallel)
-library(pdftools)
-library(KMEANS.KNN)
 
 # raw <- read.csv("/Users/rpravin/Downloads/Crime_Data_from_2010_to_2019_20241122.csv") 
 raw <- read.csv("/Users/zoelu/Downloads/Crime_Data_from_2010_to_2019_20241126.csv")
 
+# at glance view of the data
 summary(raw)
 
 # Getting rid of Records Number, Location(Street Address of Crime), 
 # Cross Street(Cross Street of Rounded Address), Latitude, Longitude
-
 raw_subset <- raw[2:24]
 
+# setting variable types
 raw_subset$AREA <- as.factor(raw_subset$AREA)
 raw_subset$AREA.NAME <- as.factor(raw_subset$AREA.NAME)
 raw_subset$Part.1.2 <- as.factor(raw_subset$Part.1.2)
-# raw_subset$Crm.Cd <- as.factor(raw_subset$Crm.Cd)
-# raw_subset$Crm.Cd.2 <- as.factor(raw_subset$Crm.Cd.2)
-# raw_subset$Crm.Cd.3 <- as.factor(raw_subset$Crm.Cd.3)
-# raw_subset$Crm.Cd.4 <- as.factor(raw_subset$Crm.Cd.4)
-# raw_subset$Mocodes <- as.factor(raw_subset$Mocodes)
 raw_subset$Vict.Sex <- as.factor(raw_subset$Vict.Sex)
 raw_subset$Vict.Descent <- as.factor(raw_subset$Vict.Descent)
-# raw_subset$Premis.Cd <- as.factor(raw_subset$Premis.Cd)
-# raw_subset$Weapon.Used.Cd <- as.factor(raw_subset$Weapon.Used.Cd)
 raw_subset$Status <- as.factor(raw_subset$Status)
 raw_subset$Status.Desc <- as.factor(raw_subset$Status.Desc)
 
+# at glance view of categorical data
 table(raw$Status.Desc)
 
 # Re-Coding Response:Status of Case ---------------------------------------
-
-
-table(raw_subset$Status.Desc)
-
-#Setting rows marked as UNK(unclear) to NA
+# Setting rows marked as UNK(unclear) to NA
 raw_subset$Status.Desc[raw_subset$Status.Desc == "UNK"] <- NA
 
-#Creating new Variable with our proposed Binary Outcome Legal Actions vs. No Legal Action(Status Case)
+# Creating new Variable with our proposed Binary Outcome Legal Actions vs. No Legal Action(Status Case)
 raw_subset$Legal_Action <- raw_subset$Status.Desc 
 raw_subset$Legal_Action <- as.character(raw_subset$Legal_Action)
 raw_subset$Legal_Action <- ifelse(raw_subset$Legal_Action == "Invest Cont", 0, 1)
 
+# counts of response variable
 table(raw_subset$Legal_Action)
 
-
-
-
 # ReCoding Date & Coding Difference in Report Time ----------------------------------------
-#Report Date
 raw_subset$date_report <- raw_subset$Date.Rptd
 raw_subset$date_report <- as.POSIXct(raw_subset$date_report, format = "%m/%d/%Y %I:%M:%S %p")
 raw_subset$date_report <- as.Date(raw_subset$date_report)
 
-#Occurence Date
+# Occurence Date
 raw_subset$date_occur <- raw_subset$DATE.OCC
 raw_subset$date_occur <- as.POSIXct(raw_subset$date_occur, format = "%m/%d/%Y %I:%M:%S %p")
 raw_subset$date_occur <- as.Date(raw_subset$date_occur)
@@ -89,9 +65,6 @@ raw_subset$date_occur <- as.Date(raw_subset$date_occur)
 raw_subset$date_occur_report_difference <- as.numeric(difftime(raw_subset$date_report, 
                                                                raw_subset$date_occur, 
                                                                units = "days"))
-
-
-
 # Categorizing Time Occurred --------------------------------------------------
 # Convert military time to string
 raw_subset$military_time <- raw_subset$TIME.OCC 
